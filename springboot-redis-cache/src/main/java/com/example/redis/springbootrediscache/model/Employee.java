1f2ac6ecff529a366b004cd249345f60f4ebdf7d
Rakeshreddy7893 <rakeshreddykalagiri@gmail.com>
2024-10-05 11:08:24 +0530

    Modified Controllers

---
+++ springboot-redis-cache/src/main/java/com/example/redis/springbootrediscache/model/Employee.java
@@ -0,0 +1,36 @@
+package com.example.redis.springbootrediscache.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+
+@Entity
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
